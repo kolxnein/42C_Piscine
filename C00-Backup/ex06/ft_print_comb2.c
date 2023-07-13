5e634612eb4d8f3a9f42213856417bdb6d20fd4b
kolxnein <115457760+kolxnein@users.noreply.github.com>
2023-07-13 15:07:46 +0300

    Add files via upload

---
+++ C00-Backup/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yozbakir <yozbakir@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 14:37:49 by yozbakir          #+#    #+#             */
+/*   Updated: 2023/07/13 14:37:52 by yozbakir          ###   ########.tr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	birinci;
+	int	ikinci;
+
+	birinci = 0;
+	ikinci = 0;
+	while (birinci <= 98)
+	{
+		ikinci = birinci + 1;
+		while (ikinci <= 99)
+		{
+			ft_putchar(birinci / 10 + '0');
+			ft_putchar(birinci % 10 + '0');
+			write(1, " ", 1);
+			ft_putchar(ikinci / 10 + '0');
+			ft_putchar(ikinci % 10 + '0');
+			if (birinci != 98 || ikinci != 99)
+			{
+				write(1, " ", 2);
+			}
+			ikinci++;
+		}
+		birinci++;
+	}
+}
