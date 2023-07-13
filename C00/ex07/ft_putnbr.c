6cd9eb10ed1486114bd98182748b64f1719302a3
kolxnein <115457760+kolxnein@users.noreply.github.com>
2023-07-13 20:12:12 +0300

    Add files via upload

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yozbakir <yozbakir@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 16:14:44 by yozbakir          #+#    #+#             */
+/*   Updated: 2023/07/13 20:10:51 by yozbakir          ###   ########.tr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+/*int main()
+{
+	ft_putnbr(-2147483647);
+}*/
